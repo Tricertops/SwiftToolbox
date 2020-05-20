311acef1423f0262707310fdf0358a3ecdefa8da
Martin Kiss <martin.kiss@me.com>
2020-05-20 12:16:37 +0200

    TimeInterval suffixes for Float literals

--- Playground.playground/Sources/Foundation/TimeInterval.swift
+++ Playground.playground/Sources/Foundation/TimeInterval.swift
@@ -37,3 +37,30 @@ extension IntegerLiteralType {
     }
 }
 
+extension FloatLiteralType {
+    
+    /// Suffix for minutes.
+    ///
+    ///     let delay = 5.min
+    ///
+    public var min: TimeInterval {
+        TimeInterval(self) * 60
+    }
+    
+    /// Suffix for seconds.
+    ///
+    ///     let delay = 30.sec
+    ///
+    public var sec: TimeInterval {
+        TimeInterval(self)
+    }
+    
+    /// Suffix for milliseconds.
+    ///
+    ///     let delay = 300.ms
+    ///
+    public var ms: TimeInterval {
+        TimeInterval(self) / 1000
+    }
+}
+
